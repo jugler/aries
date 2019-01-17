b4504ee61901bf3b10c366465c0d69a82a974a8c
Jesus Silva <jugler@gmail.com>
2019-01-16 23:42:12 -0800

    Code cleanup

--- aries.go
+++ aries.go
@@ -1,42 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
-	"net/http"
-	"html/template"
-	"strings"
 	"math/rand"
-	"encoding/json"
-
+	"net/http"
+	"strings"
 )
+
 var session ServerVars
 
-type ServerVars struct{
-	ImagesPortrait[] string
-	ImagesLanscape[] string
-	ImageBatch int
+type ServerVars struct {
+	ImagesPortrait []string
+	ImagesLanscape []string
+	ImageBatch     int
 }
 
 type Page struct {
-	Body  []byte
+	Body         []byte
 	ImageRefresh int
-	PageRefresh int
-	Images []string
-	TypePage string
+	PageRefresh  int
+	Images       []string
+	TypePage     string
 }
 
 type Config struct {
-	NextImage     bool
-	TypeOfImage   string
-	Images []string
+	NextImage   bool
+	TypeOfImage string
+	Images      []string
 }
 
-
-
 func loadPage(title string) (*Page, error) {
-	filename := title 
+	filename := title
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -49,22 +47,22 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	s := strings.SplitAfterN(url, "/", 2)
 	typePage, query := s[0], s[1]
 	log.Print("Serving: " + typePage)
-	log.Print("Query: " +  query)
+	log.Print("Query: " + query)
 	p, _ := loadPage(query)
-	if strings.Contains(query, ".htm"){
+	if strings.Contains(query, ".htm") {
 		p.Images = readImagesDir(typePage)
 		p.TypePage = typePage
 		p.PageRefresh = (p.ImageRefresh / 1000) * len(p.Images)
 		t, _ := template.ParseFiles(query)
-    	t.Execute(w, p)
-	}else if(strings.Contains(query, "configs")){
+		t.Execute(w, p)
+	} else if strings.Contains(query, "configs") {
 		fmt.Fprintf(w, "%s", getConfig(typePage))
-	}else{
+	} else {
 		fmt.Fprintf(w, "%s", p.Body)
 	}
 }
 
-func getConfig(typeConfig string)(jsonConfig []byte){	
+func getConfig(typeConfig string) (jsonConfig []byte) {
 	var config = readConfig(typeConfig)
 
 	//get Images by tags on the config
@@ -72,15 +70,14 @@ func getConfig(typeConfig string)(jsonConfig []byte){
 	//
 	jsonConfig, err := json.Marshal(config)
 	if err != nil {
-		log.Fatal( err)
+		log.Fatal(err)
 	}
 
-	
 	return jsonConfig
 }
 
-func readConfig(typeConfig string)(config Config){
-	filename := "config/" + typeConfig[0:len(typeConfig)-1] + ".config" 
+func readConfig(typeConfig string) (config Config) {
+	filename := "config/" + typeConfig[0:len(typeConfig)-1] + ".config"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -90,28 +87,27 @@ func readConfig(typeConfig string)(config Config){
 	return
 }
 
-
 func main() {
-	
+
 	http.HandleFunc("/", viewHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func loadServer()(*ServerVars){
-	return &ServerVars{ImagesPortrait: readImagesDir("portrait"), ImagesLanscape: readImagesDir("landscape"), ImageBatch:5}
+func loadServer() *ServerVars {
+	return &ServerVars{ImagesPortrait: readImagesDir("portrait"), ImagesLanscape: readImagesDir("landscape"), ImageBatch: 5}
 }
 
-func readImagesDir(directoryName string)(filenames []string){
+func readImagesDir(directoryName string) (filenames []string) {
 	dirname := "imgs/" + directoryName
 	files, err := ioutil.ReadDir(dirname)
-    if err != nil {
-        log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
 	}
 	for _, f := range files {
-		
-		filenames=append(filenames, dirname + f.Name())
+
+		filenames = append(filenames, dirname+f.Name())
 	}
-	
+
 	for i := range filenames {
 		j := rand.Intn(i + 1)
 		filenames[i], filenames[j] = filenames[j], filenames[i]
--- aries.js
+++ aries.js
@@ -1,61 +1,102 @@
-var height
-var width
-var images
-var indexCount = 0
-var toogle=true;
-var fader;
-var imageRefresh;
-
-
-
+var currentImagesLoaded=1;
+var currentImageId=0;
+var toogle=false;
 
 window.onload = function start() {
-    images = window.imageList
-    height = window.screen.availHeight + 05 //compensate for scroll bars
-    width = window.screen.availWidth - 25  //compensate for scroll bars
-    //3815x2135 
-    //vs la que segun trae 3840 x 2160
-    //25 menos que la de specs
-    //la real es
-    //3840x2120
-    resizeImgs()
-    imageRefresh = window.imageRefresh
-    fader = new Fader('fader', 2, height,width);
+    var images = window.imageList;
+    var TypePage = window.TypePage;
+    //load images
+    for (var imagesIndex=0;imagesIndex<currentImagesLoaded;imagesIndex++){
+        loadImage(imagesIndex);
+    }
+    document.getElementById(0).style.opacity=1;
+
     canvas();
 }
-function resizeImgs(){
-    for(i=0; i< images.length; i++){
-        var img = document.getElementById(i); 
-        newResolution = calculateAspectRatioFit(img.width,img.height,width,height)
-        addCount = 10
-        do{
-            newResolution = calculateAspectRatioFit(img.width,img.height,width+addCount,height+addCount)
-            addCount = addCount + 10
-        }while(newResolution.height < height || newResolution.width < width)
-        img.width = newResolution.width
-        img.height = newResolution.height
-    }
+
+function loadConfig() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var obj = JSON.parse(this.responseText);
+        if (obj.NextImage != toogle){
+            nextImage();
+            toogle = obj.NextImage;
+        }
+      }
+    };
+    xhttp.open("GET", "configs/" + window.TypePage + ".config", true);
+    xhttp.send();
+  }
+
+function loadImage(index){
+    var img = new Image();
+    img.src = window.imageList[index];
+    img.id = index;
+    img.onload= resize;
+    var div = document.getElementById("fader");
+    div.appendChild(img);
+    img.style.opacity=0;
 }
-
-function changeImage() {   
-    if (++indexCount >= images.length){
-        indexCount = 0
-    }
-
-    fader.setTarget(indexCount)
+function resize(e){
+    var img = this; 
+    newResolution = calculateAspectRatioFit(img.width, img.height, window.screen.availWidth,screen.availHeight)
+    img.width = newResolution.width
+    img.height = newResolution.height
+    //centerimage!!
+    img.style.top = (window.screen.availHeight - img.height)/2
+    img.style.left = (window.screen.availWidth - img.width)/2
 }
-
-
 
 function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
-    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+    var ratio = Math.min( (maxWidth+5) / srcWidth, (maxHeight+5) / srcHeight);
     return { width: srcWidth*ratio, height: srcHeight*ratio };
 }
 
-function canvas() {
-    window.setInterval(function () {
-        changeImage()
-    }, imageRefresh);  
+
+function nextImage(){
+    nextImageId = currentImageId+1;
+    if (nextImageId >= window.imageList.length){
+        nextImageId=0;
+    }
+    if (nextImageId > currentImagesLoaded-1){
+        currentImagesLoaded++;
+        loadImage(currentImagesLoaded-1);
+        nextImageId=currentImagesLoaded-1;
+    }
+    console.log("loading image:" +nextImageId);
+    currImg = document.getElementById(currentImageId);
+    nxtImg = document.getElementById(nextImageId);
+
+    opacityLoop(currImg,currImg.style.opacity, false);  
+    opacityLoop(nxtImg,0, true);  
+
+    currentImageId=nextImageId;
+
 }
 
 
+function opacityLoop (image,opacity,reverse) {  
+    var rate = 0.05 / window.FadeDuration;        
+    setTimeout(function () {  
+       if(!reverse){
+            image.style.opacity = opacity - rate;
+            if (image.style.opacity > 0) opacityLoop(image,opacity - rate,false);
+        }else{
+            image.style.opacity = opacity + rate;
+            if (image.style.opacity < 1) opacityLoop(image,opacity + rate,true);
+        }
+    }, 50)
+ }
+
+
+function canvas() {
+    window.setInterval(function () {
+        nextImage()
+    }, imageRefresh);  
+
+    window.setInterval(function(){
+        loadConfig()
+    },3000);
+}
