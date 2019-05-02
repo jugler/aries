866ad363290afe0afbd7dde457cf48a4ca99b649
Jesus Silva <jugler@gmail.com>
2019-05-01 23:53:48 -0700

    update to canvas not tframe

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #Send Aries code to the canvas
 echo "Uploading everything to the canvas"
-scp -r * pi@tframe:/home/pi/go/src/github.com/aries/
-scp -r config/* pi@tframe:/home/pi/go/src/github.com/aries/
+scp -r * pi@canvas:/home/pi/go/src/github.com/aries/
+scp -r config/* pi@canvas:/home/pi/go/src/github.com/aries/
 
-ssh pi@tframe 'sudo systemctl stop aries'
-ssh pi@tframe 'GOPATH=/home/pi/go go build -o /home/pi/go/src/github.com/aries/aries /home/pi/go/src/github.com/aries/app/aries.go'
-ssh pi@tframe 'sudo cp /home/pi/go/src/github.com/aries/aries.service /etc/systemd/system/aries.service'
-ssh pi@tframe 'sudo systemctl daemon-reload'
-ssh pi@tframe 'sudo systemctl start aries'
+ssh pi@canvas 'sudo systemctl stop aries'
+ssh pi@canvas 'GOPATH=/home/pi/go go build -o /home/pi/go/src/github.com/aries/aries /home/pi/go/src/github.com/aries/app/aries.go'
+ssh pi@canvas 'sudo cp /home/pi/go/src/github.com/aries/aries.service /etc/systemd/system/aries.service'
+ssh pi@canvas 'sudo systemctl daemon-reload'
+ssh pi@canvas 'sudo systemctl start aries'
