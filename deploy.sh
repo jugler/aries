6f4bd9d21c159f2e75c8228a317b70e1ded11eca
Jesus Silva <jugler@gmail.com>
2019-05-01 21:33:37 -0700

    latest changes

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ scp -r config/* pi@canvas:/home/pi/aries/config/
 
 ssh pi@canvas 'sudo systemctl stop aries'
 ssh pi@canvas 'go build -o aries/aries aries/aries.go '
+ssh pi@canvas 'sudo cp aries/aries.service /etc/systemd/system/aries.service'
+ssh pi@canvas
 ssh pi@canvas 'sudo systemctl start aries'
